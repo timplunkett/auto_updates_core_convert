a4cae058f141c33ed185ecfb6db5b21244a3000f
Tim Plunkett <git@plnktt.com>
2021-12-07 16:04:23 -0500

    Prepare for workshop

--- src/TheClass.php
+++ src/TheClass.php
@@ -8,43 +8,30 @@ use Symfony\Component\Yaml\Yaml;
 /**
  * A class to do all the things.
  */
-class TheClass {
-  public static function getSetting(string $key) {
+class TheClass
+{
+  public static function getSetting($key)
+  {
     return static::getSettings()[$key];
   }
 
-  /**
-   * @param $core_dir
-   *
-   * @return string
-   */
-  public static function getCoreModulePath(): string {
+  public static function getCoreModulePath()
+  {
     return TheClass::getSetting('core_dir') . '/core/modules/auto_updates';
   }
 
-  /**
-   * Replaces a string in the contents of the module files.
-   *
-   * @param string $search
-   * @param string $replace
-   */
-  public static function replaceContents(string $search, string $replace) {
+  public static function replaceContents($search, $replace)
+  {
     $files = static::getDirContents(static::getCoreModulePath(), TRUE);
-    foreach ($files as $file) {
+    foreach($files as $file) {
       $filePath = $file->getRealPath();
       file_put_contents($filePath,str_replace($search,$replace,file_get_contents($filePath)));
     }
 
   }
 
-  /**
-   * Renames the module files.
-   *
-   * @param string $old_pattern
-   * @param string $new_pattern
-   */
-  public static function renameFiles(string $old_pattern, string $new_pattern) {
-
+  public static function renameFiles($old_pattern, $new_pattern)
+  {
     $files = static::getDirContents(static::getCoreModulePath());
 
     // Keep a record of the files and directories to change.
@@ -54,9 +41,9 @@ class TheClass {
     // folder levels but we don't.
     $filesToChange = [];
     $dirsToChange = [];
-    foreach ($files as $file) {
+    foreach($files as $file) {
       $fileName = $file->getFilename();
-      if ($fileName === '.') {
+      if($fileName == '.') {
         $fullPath = $file->getPath();
         $parts = explode('/', $fullPath);
         $name = array_pop($parts);
@@ -66,9 +53,9 @@ class TheClass {
         $name = $fileName;
         $path = $file->getPath();
       }
-      if (strpos($name, $old_pattern) !== FALSE) {
+      if(strpos($name, $old_pattern) != FALSE) {
         $new_filename = str_replace($old_pattern, $new_pattern, $name);
-        if ($file->isFile()) {
+        if($file->isFile()) {
           $filesToChange[$file->getRealPath()] = $file->getPath() . "/$new_filename";
         }
         else {
@@ -76,30 +63,22 @@ class TheClass {
         }
       }
     }
-    foreach ($filesToChange as $old => $new) {
+    foreach($filesToChange as $old => $new) {
       (new Filesystem())->rename($old, $new);
     }
 
-    foreach ($dirsToChange as $old => $new) {
+    foreach($dirsToChange as $old => $new) {
       (new Filesystem())->rename($old, $new);
     }
-
   }
 
-  /**
-   * Gets the contents of a directory.
-   *
-   * @param string $path
-   * @param bool $excludeDirs
-   *
-   * @return \SplFileInfo[]
-   */
-  public static function getDirContents(string $path, $excludeDirs = FALSE): array {
+  public static function getDirContents($path, $excludeDirs = FALSE)
+  {
     $rii = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
 
     $files = array();
     /** @var \SplFileInfo $file */
-    foreach ($rii as $file) {
+    foreach($rii as $file) {
       if($excludeDirs && $file->isDir()) {
         continue;
       }
@@ -109,20 +88,15 @@ class TheClass {
     return $files;
   }
 
-  /**
-   * Gets the settings from config.yml.
-   *
-   * @return array
-   * @throws \Exception
-   */
-  protected static function getSettings() {
+  protected static function getSettings()
+  {
     static $settings;
-    if (!$settings) {
+    if(!$settings) {
       $settings = Yaml::parseFile(__DIR__ . '/../config.yml');
       $settings_keys = array_keys($settings);
       $require_settings = ['core_mr_branch', 'contrib_dir', 'core_dir'];
       $missing_settings = array_diff($require_settings, $settings_keys);
-      if ($missing_settings) {
+      if($missing_settings) {
         throw new \Exception('Missing settings: ' . print_r($missing_settings,
             TRUE));
       }
@@ -131,35 +105,22 @@ class TheClass {
     return $settings;
   }
 
-  /**
-   * Ensures the git status is clean.
-   *
-   * @return bool
-   * @throws \Exception
-   */
-  public static function ensureGitClean() {
+  public static function ensureGitClean()
+  {
     $status_output = shell_exec('git status');
-    if (strpos($status_output, 'nothing to commit, working tree clean') === FALSE) {
+    if(strpos($status_output, 'nothing to commit, working tree clean') == FALSE) {
       throw new \Exception("git not clean: " .$status_output);
     }
     return TRUE;
   }
 
-  /**
-   * Gets the current git branch.
-   *
-   * @return string
-   */
-  public static function getCurrentBranch() {
+  public static function getCurrentBranch()
+  {
     return trim(shell_exec('git rev-parse --abbrev-ref HEAD'));
   }
 
-  /**
-   * Switches to the branches we need.
-   *
-   * @throws \Exception
-   */
-  public static function switchToBranches() {
+  public static function switchToBranches()
+  {
     $settings = static::getSettings();
     chdir($settings['contrib_dir']);
     static::switchToBranch('8.x-2.x');
@@ -167,22 +128,17 @@ class TheClass {
     static::switchToBranch($settings['core_mr_branch']);
   }
 
-  /**
-   * Switches to a branches and makes sure it is clean.
-   *
-   * @param string $branch
-   *
-   * @throws \Exception
-   */
-  public static function switchToBranch(string $branch) {
+  public static function switchToBranch($branch)
+  {
     static::ensureGitClean();
     shell_exec("git checkout $branch");
-    if ($branch !== static::getCurrentBranch()) {
+    if($branch != static::getCurrentBranch()) {
       throw new \Exception("could not check $branch");
     }
   }
 
-  public static function makeCommit() {
+  public static function makeCommit()
+  {
     chdir(self::getSetting('contrib_dir'));
     self::ensureGitClean();
     $hash = trim(shell_exec('git rev-parse HEAD'));
